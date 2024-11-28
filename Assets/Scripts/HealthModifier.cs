ea4324526ff303a03f2ad7a3c689e7a5fe9bf1bf
shadowfinderstudios <83621064+shadowfinderstudios@users.noreply.github.com>
2024-11-27 21:27:06 -0600

    Added SFX for the heal and damage zones.
    Heal and damage zones now have audio effects.

--- Assets/Scripts/HealthModifier.cs
+++ Assets/Scripts/HealthModifier.cs
@@ -7,10 +7,17 @@ public class HealthModifier : NetworkBehaviour
     [SerializeField] int _healthModifier = 1;
     [SerializeField] bool _affectWhilePresent = false;
     [SerializeField] float _frequency = 0.5f;
+    [SerializeField] AudioClip _audioClip;
 
     bool _isPresent = false;
     Collider2D _collision;
     float _timer = 0f;
+    AudioSource _audioSource;
+
+    void OnEnable()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
@@ -23,6 +30,7 @@ public class HealthModifier : NetworkBehaviour
             }
 
             ModifyHealth(collision.gameObject, _healthModifier);
+            _audioSource?.PlayOneShot(_audioClip);
         }
     }
 
