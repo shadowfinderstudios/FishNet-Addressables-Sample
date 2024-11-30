18247af6efa996ab248ce6d83e5cfb8ef9686ee0
shadowfinderstudios <83621064+shadowfinderstudios@users.noreply.github.com>
2024-11-30 05:10:42 -0600

    Removed GOAP
    GOAP has been removed in anticipation of developing an alternative solution that will be more friendly using Behavior Trees going forward.

--- Assets/Scripts/GOAP/TargetKeys/IdleTarget.cs
+++
@@ -1,7 +0,0 @@
-using CrashKonijn.Goap.Runtime;
-
-namespace CrashKonijn.Goap.GenTest
-{
-    [GoapId("IdleTarget-9b65ebfb-44ba-42eb-ab3a-7fb7f7347432")]
-    public class IdleTarget : TargetKeyBase {}
-}
--- Assets/Scripts/GOAP/WorldKeys/IsIdle.cs
+++
@@ -1,7 +0,0 @@
-using CrashKonijn.Goap.Runtime;
-
-namespace CrashKonijn.Goap.GenTest
-{
-    [GoapId("IsIdle-e2378cc8-14c9-4c79-a1ea-082a4b1ac585")]
-    public class IsIdle : WorldKeyBase {}
-}
