a79bc098044d0751de79771f520b44f23da80843
shadowfinderstudios <83621064+shadowfinderstudios@users.noreply.github.com>
2024-12-02 11:37:10 -0600

    Added unauthenticated chat
    Names are generated randomly using the clientid as a seed, without authentication. For testing purposely only.

---
+++ Assets/Scripts/General/ChatBroadcast.cs
@@ -0,0 +1,7 @@
+using FishNet.Broadcast;
+
+public struct ChatBroadcast : IBroadcast
+{
+    public string Username;
+    public string Message;
+}
--- Assets/Scripts/General/ChatSystem.cs
+++ Assets/Scripts/General/ChatSystem.cs
@@ -1,3 +1,6 @@
+using FishNet;
+using FishNet.Connection;
+using FishNet.Transporting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,7 +28,19 @@ public class ChatSystem : MonoBehaviour
         }
     }
 
-    private void Update()
+    void OnEnable()
+    {
+        InstanceFinder.ClientManager.RegisterBroadcast<ChatBroadcast>(OnClientBroadcast);
+        InstanceFinder.ServerManager.RegisterBroadcast<ChatBroadcast>(OnServerBroadcast, false);
+    }
+
+    void OnDisable()
+    {
+        InstanceFinder.ClientManager.UnregisterBroadcast<ChatBroadcast>(OnClientBroadcast);
+        InstanceFinder.ServerManager.UnregisterBroadcast<ChatBroadcast>(OnServerBroadcast);
+    }
+
+    void Update()
     {
         if (Input.GetKeyDown(KeyCode.Return))
         {
@@ -37,15 +52,42 @@ public class ChatSystem : MonoBehaviour
 
     public void OnEndEdit(string text)
     {
-        if (text == "") return;
+        var msg = new ChatBroadcast() { Message = text };
+        InstanceFinder.ClientManager.Broadcast(msg);
+    }
+
+    bool UpdateChat(string username, string message)
+    {
+        if (string.IsNullOrEmpty(username)) return false;
+        if (string.IsNullOrEmpty(message)) return false;
 
         if (_history.Count >= _maxLines) _history.RemoveAt(0);
-        _history.Add("You: " + text);
+        _history.Add(username + ": " + message);
 
         _chatText.text = string.Join("\n", _history.ToArray());
         _inputField.text = "";
 
         if (_history.Count > _linesToScrollAt)
             _scrollRect.normalizedPosition = new Vector2(0, 0);
+        return true;
+    }
+
+    private void OnServerBroadcast(NetworkConnection conn, ChatBroadcast msg, Channel channel)
+    {
+        var nob = conn.FirstObject;
+        if (nob == null) return;
+        msg.Username = GetClientUsername(conn);
+        InstanceFinder.ServerManager.Broadcast(nob, msg, true);
+    }
+
+    private void OnClientBroadcast(ChatBroadcast msg, Channel channel)
+    {
+        UpdateChat(msg.Username, msg.Message);
+    }
+
+    private string GetClientUsername(NetworkConnection conn)
+    {
+        var generator = new ElvenNameGenerator.ElvenNameGenerator(conn.ClientId);
+        return generator.GenerateName();
     }
 }
